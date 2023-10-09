3330847e74934fec52bcdadae5aafe497d7d2ed3
cipher <asaforbrian18@gmail.com>
2023-10-09 15:02:22 +0300

    created the Student Model

--- student_management_system/student_management_app/models.py
+++ student_management_system/student_management_app/models.py
@@ -9,3 +9,12 @@ class Admin(models.Model):
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now_add=True)
     objects=models.Manager()
+    
+class Student(models.Model):
+    id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=255)
+    email=models.CharField(max_length=255)
+    password=models.CharField(max_length=255)
+    gender=models.CharField(max_length=255)
+    profile_pic=models.FileField()
+    address=models.TextField()
