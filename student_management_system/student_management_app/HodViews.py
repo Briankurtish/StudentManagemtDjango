4e93e27fdee665d212f529ffa5020936872b7e05
cipher <asaforbrian18@gmail.com>
2023-10-10 01:43:39 +0300

    Working on the Add Staff page and successfully added a staff to the database

--- student_management_system/student_management_app/HodViews.py
+++ student_management_system/student_management_app/HodViews.py
@@ -1,4 +1,8 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import messages
 from django.shortcuts import render
+
+from student_management_app.models import CustomUser
 
 
 def admin_home(request):
@@ -6,3 +10,26 @@ def admin_home(request):
 
 def add_staff(request):
     return render(request, "hod_template/add_staff_template.html")
+
+def add_staff_save(request):
+    if request.method!="POST":
+        return HttpResponse("Method not Allowed")
+    else:
+        first_name=request.POST.get("first_name")
+        last_name=request.POST.get("last_name")
+        username=request.POST.get("username")
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+        address=request.POST.get("address")
+        try:
+            user=CustomUser.objects.create_user(username=username, password=password, email=email , first_name=first_name, last_name=last_name, user_type=2)
+            user.staffs.address=address
+            user.save()
+            messages.success(request, "Staff Added Successfully")
+            return HttpResponseRedirect("/add_staff")
+            
+        except:
+            messages.error(request, "Failed to Add Staff")
+            return HttpResponseRedirect("/add_staff") 
+        
+        
