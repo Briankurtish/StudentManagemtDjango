21192eee2ed71df7a2e3f535ea1e97212669bcce
cipher <asaforbrian18@gmail.com>
2023-10-09 22:49:42 +0300

    created login functions and successfully logged in and log out

--- student_management_system/student_management_app/views.py
+++ student_management_system/student_management_app/views.py
@@ -1,4 +1,8 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+
+from student_management_app.EmailBackEnd import EmailBackEnd
 
 # Create your views here.
 
@@ -8,3 +12,26 @@ def showDemoPage(request):
 
 def ShowLoginPage(request):
     return render(request, "login_page.html")
+
+def doLogin(request):
+    if request.method!="POST":
+        return HttpResponse("<h2>Method Not Allowed</h2>")
+    else:
+        user=EmailBackEnd.authenticate(request, username=request.POST.get("email"), password=request.POST.get("password"))
+        if user!=None:
+            login(request, user)
+            return HttpResponse("Email : "+request.POST.get("email")+" Password : "+request.POST.get("password"))
+        else:
+            return HttpResponse("Invalid Login")
+
+
+def GetUserDetails(request):
+    if request.user!=None:
+        return HttpResponse("User : "+request.user.email+" user type : "+request.user.user_type)
+    else:
+        return HttpResponse("Please Login First")
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect("/")
+    
