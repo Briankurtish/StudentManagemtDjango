9bc23f783c4393ea5a17c78a6762c9dc14d7a458
cipher <asaforbrian18@gmail.com>
2023-10-09 16:44:57 +0300

    added css and js files and modified the main urls.py file to hold static files and media

--- student_management_system/student_management_system/urls.py
+++ student_management_system/student_management_system/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+
+from student_management_system.student_management_system import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
