7e26abb804dfb264c56a41506aeae98991d541c3
cipher <asaforbrian18@gmail.com>
2023-10-09 21:12:11 +0300

    added the login page and configured it to display

--- student_management_system/student_management_app/views.py
+++ student_management_system/student_management_app/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def showDemoPage(request):
     return render(request, 'demo.html')
+
+
+def ShowLoginPage(request):
+    return render(request, "login_page.html")
