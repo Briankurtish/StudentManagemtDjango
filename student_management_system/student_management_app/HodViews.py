0b4047e0e1a285dc75cf0baca4282cf74d5ac3ba
cipher <asaforbrian18@gmail.com>
2023-10-10 03:27:59 +0300

    Working on the Add Srudent page and successfully added a Student to the database

--- student_management_system/student_management_app/HodViews.py
+++ student_management_system/student_management_app/HodViews.py
@@ -53,7 +53,36 @@ def add_course_save(request):
         
 
 def add_student(request):
-    return render(request, "hod_template/add_student_template.html")
+    courses=Courses.objects.all()
+    return render(request, "hod_template/add_student_template.html", {"courses":courses})
 
 def add_student_save(request):
-    pass
+    if request.method!="POST":
+        return HttpResponse("Method not Allowed")
+    else:
+        first_name=request.POST.get("first_name")
+        last_name=request.POST.get("last_name")
+        username=request.POST.get("username")
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+        address=request.POST.get("address")
+        session_start=request.POST.get("session_start")
+        session_end=request.POST.get("session_end")
+        course_id=request.POST.get("course")
+        gender=request.POST.get("gender")
+        try:
+            user=CustomUser.objects.create_user(username=username, password=password, email=email , first_name=first_name, last_name=last_name, user_type=3)
+            user.students.address=address
+            course_obj=Courses.objects.get(id=course_id)
+            user.students.course_id=course_obj
+            user.students.session_start_year=session_start
+            user.students.session_end_year=session_end
+            user.students.gender=gender
+            user.students.profile_pic=""
+            user.save()
+            messages.success(request, "Student Added Successfully")
+            return HttpResponseRedirect("/add_staff")
+            
+        except:
+            messages.error(request, "Failed to Add Student")
+            return HttpResponseRedirect("/add_student") 
